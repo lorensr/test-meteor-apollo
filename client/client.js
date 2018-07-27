e120f6412c20fa7d13d8f1d728ff7772583de3ac
Loren 🤓 <lorensr@gmail.com>
2018-07-27 01:06:53 -0400

    Use MeteorAccountsLink()

--- client/client.js
+++ client/client.js
@@ -1,12 +1,28 @@
-import { Accounts } from 'meteor/accounts-base'
-import ApolloClient from 'apollo-boost'
+// import { Accounts } from 'meteor/accounts-base'
+// import ApolloClient from 'apollo-boost'
+
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   request: operation =>
+//     operation.setContext(() => ({
+//       headers: {
+//         authorization: Accounts._storedLoginToken()
+//       }
+//     }))
+// })
+
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { MeteorAccountsLink } from 'meteor/apollo'
 
 const client = new ApolloClient({
-  uri: '/graphql',
-  request: operation =>
-    operation.setContext(() => ({
-      headers: {
-        authorization: Accounts._storedLoginToken()
-      }
-    }))
+  link: ApolloLink.from([
+    new MeteorAccountsLink(),
+    new HttpLink({
+      uri: '/graphql'
+    })
+  ]),
+  cache: new InMemoryCache()
 })
